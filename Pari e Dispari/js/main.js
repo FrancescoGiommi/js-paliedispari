8e5977bccbad021d46bcceff46af41bf2007240e
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-18 17:42:06 +0200

    Add comment

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -7,16 +7,26 @@ Dichiariamo chi ha vinto.
 
 /* RACCOLTA DATI */
 let userNumber;
-const choiceNumber = prompt('Scegli un numero "Pari" o "Dispari ');
-console.log(choiceNumber);
+
 /* PROCEDURA */
 /* ELABORAZIONE */
+/* chiedo all'utente di scegliere un numero pari o dispari */
+const choiceNumber = prompt('Scegli un numero "Pari" o "Dispari" tra 1 e 5 ');
+console.log(choiceNumber);
 
+/* genero un numero random per il computer con una funzione */
+
+/* faccio somma i 2 numeri */
+
+/* stabilisco se la somma dei numeri è pari o dospari con una funzione */
+
+/* dichiaro il vincitore */
 /* OUTPUT */
 
 /*Consigli del giorno
 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.Domande da  farsi quando si crea una funzione:
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+Domande da  farsi quando si crea una funzione:
 1. Come dovrebbe chiamarsi?
 2. Ho bisogno di parametri?
 3. Devo restituire un valore?
