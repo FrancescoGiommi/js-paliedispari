3ab7a1afbb6c23b5dc4a8cb99e5bb2b007898017
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-18 17:35:43 +0200

    Add choice number

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -3,7 +3,18 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.Consigli del giorno
+Dichiariamo chi ha vinto.
+
+/* RACCOLTA DATI */
+let userNumber;
+const choiceNumber = prompt('Scegli un numero "Pari" o "Dispari ');
+console.log(choiceNumber);
+/* PROCEDURA */
+/* ELABORAZIONE */
+
+/* OUTPUT */
+
+/*Consigli del giorno
 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.Domande da  farsi quando si crea una funzione:
 1. Come dovrebbe chiamarsi?
