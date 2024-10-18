55cf0891f4433dac06d9e02d45fd038be9ecb5d2
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-18 15:31:16 +0200

    Add wordRequest

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -2,10 +2,19 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
+/* RACCOLTA DATI */
 
-Consigli del giorno
+/* PROCEDURA */
+/* ELABORAZIONE */
+/* chiedo all'utente di inserie una parola  */
+const wordRequest = prompt("Inserisci una parola");
+/* OUTPUT */
+
+/*Consigli del giorno
 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.Domande da  farsi quando si crea una funzione:
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.*/
+
+/*Domande da  farsi quando si crea una funzione:
 1. Come dovrebbe chiamarsi?
 2. Ho bisogno di parametri?
 3. Devo restituire un valore?
